4008eb75631cf0304a64286b1d4345ab05a5de42
Nishar0007 <90242522+Nishar0007@users.noreply.github.com>
2021-09-07 14:35:36 +0530

    Update GetProducts.php
    updated valuable variable

--- app/code/Nishar/StockQty/Block/GetProducts.php
+++ app/code/Nishar/StockQty/Block/GetProducts.php
@@ -8,23 +8,23 @@ class GetProducts extends \Magento\Framework\View\Element\Template
 {
     public $registry;
     public $context;
-    public $StockState;
+    public $stockState;
     public $product;
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         \Magento\Framework\Registry $registry,
-        \Magento\CatalogInventory\Api\StockStateInterface $StockState,
+        \Magento\CatalogInventory\Api\StockStateInterface $stockState,
         array $data = []
     ) {
         $this->registry = $registry;
-        $this->StockState = $StockState;
+        $this->stockState = $stockState;
         parent::__construct($context, $data);
     }
 
     public function getStock()
     {
         $product = $this->registry->registry('current_product');
-        return $this->StockState->getStockQty($product->getId());
+        return $this->stockState->getStockQty($product->getId());
     }
 
 }

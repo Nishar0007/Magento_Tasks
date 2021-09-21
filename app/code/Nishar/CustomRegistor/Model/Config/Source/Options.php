8da1aced18b39894eab559c2ae7de96b2889ecda
Nishar0007 <90242522+Nishar0007@users.noreply.github.com>
2021-09-21 19:56:39 +0530

    updated value as integer

--- app/code/Nishar/CustomRegistor/Model/Config/Source/Options.php
+++ app/code/Nishar/CustomRegistor/Model/Config/Source/Options.php
@@ -12,8 +12,8 @@ class Options extends \Magento\Eav\Model\Entity\Attribute\Source\AbstractSource
     public function getAllOptions()
     {
         $this->_options = [
-            ['label' => __('Type One'), 'value' => 'Type One'],
-            ['label' => __('Type Two'), 'value' => 'Type Two'],
+            ['label' => __('Type One'), 'value' => '0'],
+            ['label' => __('Type Two'), 'value' => '1'],
         ];
 
         return $this->_options;
